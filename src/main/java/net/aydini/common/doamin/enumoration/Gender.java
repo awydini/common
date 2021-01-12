734abcd423a381917a02fe48cfae27c26f5d968b
Aydin Nasrollahpour <paakro@gmail.com>
2021-01-13 00:10:37 +0330

    add gender

---
+++ src/main/java/net/aydini/common/doamin/enumoration/Gender.java
@@ -0,0 +1,13 @@
+package net.aydini.common.doamin.enumoration;
+
+/**
+ * 
+ * @author  <a href="mailto:hi@aydini.net">Aydin Nasrollahpour </a>
+ *
+ *Jan 13, 2021
+ */
+public enum Gender {
+    
+    MALE,FEMAIL;
+
+}
