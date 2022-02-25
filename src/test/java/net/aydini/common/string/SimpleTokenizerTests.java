07f57c28002c2156a4e3b9dcbd69c1a2c2414180
Aydin Nasrollahpour <paakro@gmail.com>
2022-02-25 23:40:13 +0330

    refactor

--- src/test/java/net/aydini/common/string/SimpleTokenizerTests.java
+++ src/test/java/net/aydini/common/string/SimpleTokenizerTests.java
@@ -1,10 +1,10 @@
 package net.aydini.common.string;
 
+import net.aydini.common.util.SimpleTokenizer;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.NullSource;
 
 import java.util.NoSuchElementException;
